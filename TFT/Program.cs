fa444975aab95edd81945cc45c4424234f6043e1
Vaso Yiannopoulou <vasw.gian@hotmail.com>
2023-01-17 15:58:36 +0200

    Final stage

--- TFT/Program.cs
+++ TFT/Program.cs
@@ -13,12 +13,16 @@ builder.Services.AddHttpClient<RiotHttpClient>(config =>
 {
     config.BaseAddress = new Uri("https://eun1.api.riotgames.com");
     config.Timeout = new TimeSpan(0, 0, 45);
+<<<<<<< Updated upstream
     config.DefaultRequestHeaders.Add("X-Riot-Token", "RGAPI-c26e79d4-9610-47c5-ac0d-a17126cfe1f4");
 });
 builder.Services.AddHttpClient<TftHttpClient>(config => {
     config.BaseAddress = new Uri("https://eun1.api.riotgames.com");
     config.Timeout = new TimeSpan(0, 0, 45);
     config.DefaultRequestHeaders.Add("X-Riot-Token", "RGAPI-c26e79d4-9610-47c5-ac0d-a17126cfe1f4");
+=======
+    config.DefaultRequestHeaders.Add("X-Riot-Token", "RGAPI-0f306110-22c1-4823-af28-b416174fbc91");
+>>>>>>> Stashed changes
 });
 
 
